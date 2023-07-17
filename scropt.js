3cc46fa3f1c8e4e2ff537b9f4612ad6e2f5ba01e
Hello World <88145693+byte1001@users.noreply.github.com>
2023-07-17 10:45:44 +0800

    Update scropt.js

--- scropt.js
+++ scropt.js
@@ -3,6 +3,7 @@ function buttonRedirect(url) {
     p1 = prompt("p1")
     p2 = prompt("p2")
     window.open(`https://${p1}.netlify.app/${p2}`,"_blank")
+    return ;
   }
   window.location.href = `https://byte1001.github.io/${url}`;
 }
