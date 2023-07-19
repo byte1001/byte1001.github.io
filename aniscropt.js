d78b0c55a1a78800b1e223aeef4193b5f5a04cfb
Hello World <88145693+byte1001@users.noreply.github.com>
2023-07-19 21:36:54 +0800

    Create aniscropt.js

---
+++ aniscropt.js
@@ -0,0 +1,17 @@
+let animation = anime({
+  targets: '.iam',
+  duration: 1000,
+  easing: 'easeInOutSine',
+  loop: false,
+  translateX: [-1000,0],
+  hidden: false
+});
+
+let animation2 = anime({
+  targets: '.byte1001dot',
+  duration: 1000,
+  easing: 'easeInOutSine',
+  loop: false,
+  translateX: [5000,0],
+  hidden: false
+});
